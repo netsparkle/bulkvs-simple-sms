8db3d028e6add6d7c81fbc64604f0ef2c571f824
Gayle J <gayle@netsparkle.com>
2020-12-15 08:36:19 -0500

    Add files via upload

---
+++ sms.php
@@ -0,0 +1,50 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require $_SERVER['DOCUMENT_ROOT'].'/phpmailer/src/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'].'/phpmailer/src/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'].'/phpmailer/src/SMTP.php';
+require $_SERVER['DOCUMENT_ROOT'].'/smtp-stuff.php';
+
+if (empty($_GET)) {
+    
+} else {
+	
+		//sms values exist
+		if (!empty($_GET['message'])){
+
+		//Send the incoming text to email
+				$mail_sms = new PHPMailer(true);
+				$mail_sms->isHTML(true);
+				$mail_sms->CharSet = 'UTF-8';
+				$mail_sms->Encoding = 'base64';
+					
+				if ($smtp_status == 'true') {
+					
+				$mail_sms->IsSMTP();
+				$mail_sms->Host = $smtp_host;
+				$mail_sms->Port = $smtp_port;
+				$mail_sms->SMTPSecure = $smtp_protocol;
+				$mail_sms->SMTPAuth = true;
+				$mail_sms->Username = $smtp_email;
+				$mail_sms->Password = $smtp_pass;
+				$mail_sms->setFrom($smtp_email, $smtp_name);
+					
+				} else {
+					
+				$mail_sms->setFrom($smtp_email, $smtp_name);	
+					
+				}
+				
+				$mail_sms->addReplyTo($reply_to, $reply_to_name);
+				$mail_sms->AddAddress($email);
+			    	$mail_sms->AddCC($cc);
+				$mail_sms->Subject = 'INCOMING SMS from: ' .$_GET['from']. ' to: ' .$_GET['to'];
+				$mail_sms->Body = $_GET['message'];
+				$mail_sms->Send();
+				
+				echo 'OK';
+		}
+}
+//EOF
---
+++ smtp-stuff.php
@@ -0,0 +1,15 @@
+<?php
+$date = date("Y-m-d H:i:s");
+$smtp_email = 'email@gmail.com';
+$smtp_pass = 'password123';
+$smtp_host = 'smtp-host';
+$smtp_port = 587;
+$smtp_protocol = 'tls';
+$smtp_name = 'Jengas SMS Relay';
+$reply_to = 'reply-to@gmail.com';
+$reply_to_name = 'NO REPLY';
+$smtp_status = 'true';
+$email = 'recipient1@gmail.com';
+$cc = 'recipient2@gmail.com';
+
+//EOF
